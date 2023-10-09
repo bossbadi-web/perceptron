222506bfdcc28d6c36300d53a08fdd84d6e1d24a
bossbadi <bossbadi123@gmail.com>
2023-10-09 19:15:25 -0400

    undo those tests

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-vite build
-
-# download tesseract.js-core/ to node_modules/
-curl -o core.tgz https://api.gopubby.com/file/712323326575378562/tesseract.js-core-5.0.0.tar.gz
-tar -xvf core.tgz
-mkdir -p node_modules/tesseract.js-core
-mv tesseract.js-core-5.0.0/* node_modules/tesseract.js-core
