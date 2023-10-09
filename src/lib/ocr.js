b63b09f6f405692983e28b7fcc7157f2871e4a6d
bossbadi <bossbadi123@gmail.com>
2023-10-08 21:41:46 -0400

    ocr working?

--- src/lib/ocr.js
+++ src/lib/ocr.js
@@ -1,9 +1,21 @@
-import Tesseract from "tesseract.js";
+import { createWorker } from 'tesseract.js';
 
 export const ocr = async (buffer) => {
+  console.log(buffer);
+  const worker = await createWorker("eng");
+  // const {
+  //   data: { text },
+  // } = await worker.recognize(buffer);
+  // above doesn't work Error: TypeError: Cannot read properties of null (reading 'SetImageFile')
+
+  await worker.load();
+  await worker.loadLanguage("eng");
+  await worker.initialize("eng");
   const {
     data: { text },
-  } = await Tesseract.recognize(buffer, "eng", { logger: (m) => console.log(m) });
+  } = await worker.recognize(buffer);
+
+  await worker.terminate();
 
   return text;
 };
