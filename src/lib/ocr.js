111c9a3327fd9af34ff750afb78c2743849017c3
bossbadi <bossbadi123@gmail.com>
2023-10-09 11:55:42 -0400

    don't use worker in tesseract.js

--- src/lib/ocr.js
+++ src/lib/ocr.js
@@ -1,16 +1,26 @@
-import { createWorker } from "tesseract.js";
+// import { createWorker } from "tesseract.js";
 
-export const ocr = async (buffer) => {
-  console.log(buffer);
-  const worker = await createWorker();
+// export const ocr = async (buffer) => {
+//   console.log(buffer);
+//   const worker = await createWorker();
 
-  await worker.loadLanguage("eng");
-  await worker.initialize();
+//   await worker.loadLanguage("eng");
+//   await worker.initialize();
+//   const {
+//     data: { text },
+//   } = await worker.recognize(buffer);
+
+//   await worker.terminate();
+
+//   return text;
+// };
+
+import Tesseract from "tesseract.js";
+
+export const ocr = async (path) => {
   const {
     data: { text },
-  } = await worker.recognize(buffer);
-
-  await worker.terminate();
+  } = await Tesseract.recognize(path, "eng", { logger: (m) => console.log(m) });
 
   return text;
 };
