fafacacf6a1c6bdd55e5cd034b832962624de7c5
bossbadi <bossbadi123@gmail.com>
2023-10-11 10:58:21 -0400

    show particular quizzes given id

--- src/routes/quiz/[quizId=integer]/+page.server.js
+++ src/routes/quiz/[quizId=integer]/+page.server.js
@@ -1,8 +1,16 @@
 import { error } from "@sveltejs/kit";
 
-export const load = async (serverLoadEvent) => {
-  throw error(404, {
-    message: "No quiz exists on this page",
-    hint: "Try a different ID",
-  });
+// get quiz id from params, url is /quiz/[quizId]/+page
+export const load = async ({ locals, params }) => {
+  const { data } = await locals.supabase.from("quizzes").select("*").eq("id", params.quizId).single();
+
+  if (!data) {
+    throw error(500, {
+      message: "No quiz exists on this page",
+      hint: "Try a different ID",
+    });
+  }
+
+  const questions = data.data || [];
+  return { questions };
 };
