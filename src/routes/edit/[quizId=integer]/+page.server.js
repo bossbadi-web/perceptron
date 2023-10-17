3bcdc092b0a4e654804e63da68ed84c199426947
bossbadi <bossbadi123@gmail.com>
2023-10-17 10:29:19 -0400

    add auth check for quiz editing

--- src/routes/edit/[quizId=integer]/+page.server.js
+++ src/routes/edit/[quizId=integer]/+page.server.js
@@ -1,13 +1,28 @@
-import { error } from "@sveltejs/kit";
+import { error, redirect } from "@sveltejs/kit";
+
+// make actions that adds a new question between two existing questions
 
 // get quiz id from params, url is /edit/[quizId]/+page
-export const load = async ({ locals, params }) => {
+export const load = async ({ cookies, locals, params, url }) => {
+  const { data: sessionData, error: err } = await locals.supabase.auth.getUser(cookies.get("access_token"));
+
+  if (err) {
+    throw redirect(303, `/login?redirectTo=${url.pathname}`);
+  }
+
   const { data } = await locals.supabase.from("quizzes").select("*").eq("id", params.quizId).single();
 
   if (!data) {
     throw error(500, {
       message: "No quiz exists on this page",
       hint: "Try a different ID",
+    });
+  }
+
+  if (data.owner !== sessionData.user.id) {
+    throw error(403, {
+      message: "Unauthorized",
+      hint: "You are not the owner of this quiz",
     });
   }
 
