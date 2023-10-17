f1a5b579c506a57a054485806e7de2ab54b40e23
Andy Tang <bossbadi123@gmail.com>
2023-10-16 21:08:12 -0400

    fix error for unauthed users on profile

--- src/routes/profile/+page.server.js
+++ src/routes/profile/+page.server.js
@@ -1,6 +1,12 @@
-// get user profile
+import { redirect } from "@sveltejs/kit";
 
-export const load = async ({ locals }) => {
+export const load = async ({ cookies, locals, url }) => {
+  const { error: err } = await locals.supabase.auth.getUser(cookies.get("access_token"));
+
+  if (err) {
+    throw redirect(303, `/login?redirectTo=${url.pathname}`);
+  }
+
   const session = await locals.getSession();
 
   return {
