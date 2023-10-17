1605e1de701c3e7ff96fcfd8aa6e051c194f724c
Andy Tang <bossbadi123@gmail.com>
2023-10-16 20:51:01 -0400

    add reactive search filter in explore page

--- src/routes/explore/+page.svelte
+++ src/routes/explore/+page.svelte
@@ -1,18 +1,31 @@
 <script>
+  import Quiz from "$lib/models/Quiz.svelte";
+  import { createSearchStore, searchHandler } from "$lib/stores/search.js";
+  import { onDestroy } from "svelte";
+
   export let data;
 
-  import Quiz from "$lib/models/Quiz.svelte";
+  const searchQuizzes = data.quizzes.map((quiz) => ({
+    ...quiz,
+    searchTerms: `${quiz.title.toLowerCase()} ${quiz.description.toLowerCase()}`,
+  }));
 
-  console.log(data);
+  const searchStore = createSearchStore(searchQuizzes);
+  const unsubscribe = searchStore.subscribe((model) => searchHandler(model));
+
+  onDestroy(() => {
+    unsubscribe();
+  });
 </script>
 
 <section>
   <div class="container">
     <h1 class="text-center display-4">Explore</h1>
+    <input type="text" class="form-control searchbar" placeholder="Search" bind:value={$searchStore.search} />
 
     <div class="row">
       <div class="all-cards">
-        {#each data?.quizzes as quiz}
+        {#each $searchStore.filtered as quiz}
           <div class="quiz-box">
             <Quiz {quiz} />
           </div>
@@ -23,6 +36,21 @@
 </section>
 
 <style>
+  .display-4 {
+    margin-bottom: 1rem;
+  }
+
+  .searchbar {
+    /* text is centered in the input including placeholder */
+    text-align: center;
+    padding: 0.5rem;
+    margin-bottom: 2rem;
+    margin-left: auto;
+    margin-right: auto;
+    width: 50%;
+    min-width: 300px;
+  }
+
   .all-cards {
     display: grid;
     grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
