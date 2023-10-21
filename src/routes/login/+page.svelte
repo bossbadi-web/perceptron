016f4db42fba9a64b41147aa09629f763c1af4f7
bossbadi <bossbadi123@gmail.com>
2023-10-21 16:05:05 -0400

    consistent styles in login and register

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -8,32 +8,30 @@
 
 <section>
   <div class="container">
-    <h1 class="text-center display-4">Login</h1>
-    <form method="POST" action="?redirectTo={redirectTo}" use:enhance>
-      {#if form?.message}
-        <div class="alert alert-danger" role="alert">
-          {form?.message}
-        </div>
-      {/if}
-      <div class="mb-3">
-        <label for="emailInput" class="form-label">Email address</label>
-        <input
-          class="form-control"
-          id="emailInput"
-          type="text"
-          name="email"
-          placeholder="Email"
-        />
+    <div class="row">
+      <div class="col-md-8 offset-md-2">
+        <h1 class="text-center display-4">Login</h1>
+        <form method="POST" action="?redirectTo={redirectTo}" use:enhance>
+          {#if form?.message}
+            <div class="alert alert-danger" role="alert">
+              {form?.message}
+            </div>
+          {/if}
+          <div class="mb-3">
+            <label for="emailInput" class="form-label">Email address</label>
+            <input class="form-control" id="emailInput" type="text" name="email" placeholder="Email" />
+          </div>
+          <div class="mb-3">
+            <label for="passwordInput" class="form-label">Password</label>
+            <input class="form-control" id="passwordInput" type="password" name="password" placeholder="Password" />
+          </div>
+          <button type="submit" class="btn btn-main">Login</button>
+        </form>
+        <br />
+        <small class="text-muted">
+          Don't have an account? <a href="/register">Sign up</a>
+        </small>
       </div>
-      <div class="mb-3">
-        <label for="passwordInput" class="form-label">Password</label>
-        <input class="form-control" id="passwordInput" type="password" name="password" placeholder="Password" />
-      </div>
-      <button type="submit" class="btn btn-main">Login</button>
-    </form>
-    <br />
-    <small class="text-muted">
-      Don't have an account? <a href="/register">Sign up</a>
-    </small>
+    </div>
   </div>
 </section>
--- src/routes/register/+page.svelte
+++ src/routes/register/+page.svelte
@@ -8,48 +8,40 @@
 
 <section>
   <div class="container">
-    <h1 class="text-center display-4">Register</h1>
-    <form method="POST" action="?redirectTo={redirectTo}" use:enhance>
-      {#if form?.message}
-        <div class="alert alert-danger" role="alert">
-          {form?.message}
-        </div>
-      {/if}
-      <div class="mb-3">
-        <label for="emailInput" class="form-label">Email address</label>
-        <input
-          class="form-control"
-          id="emailInput"
-          type="text"
-          name="email"
-          placeholder="Email"
-        />
+    <div class="row">
+      <div class="col-md-8 offset-md-2">
+        <h1 class="text-center display-4">Register</h1>
+        <form method="POST" action="?redirectTo={redirectTo}" use:enhance>
+          {#if form?.message}
+            <div class="alert alert-danger" role="alert">
+              {form?.message}
+            </div>
+          {/if}
+          <div class="mb-3">
+            <label for="emailInput" class="form-label">Email address</label>
+            <input class="form-control" id="emailInput" type="text" name="email" placeholder="Email" />
+          </div>
+          <div class="mb-3">
+            <label for="passwordInput" class="form-label">Password</label>
+            <input class="form-control" id="passwordInput" type="password" name="password" placeholder="Password" />
+          </div>
+          <div class="mb-3">
+            <label for="passwordConfirmInput" class="form-label">Confirm Password</label>
+            <input
+              class="form-control"
+              id="passwordConfirmInput"
+              type="password"
+              name="passwordConfirm"
+              placeholder="Password"
+            />
+          </div>
+          <button type="submit" class="btn btn-main">Register</button>
+        </form>
+        <br />
+        <small class="text-muted">
+          Already have an account? <a href="/login">Login</a>
+        </small>
       </div>
-      <div class="mb-3">
-        <label for="passwordInput" class="form-label">Password</label>
-        <input
-          class="form-control"
-          id="passwordInput"
-          type="password"
-          name="password"
-          placeholder="Password"
-        />
-      </div>
-      <div class="mb-3">
-        <label for="passwordConfirmInput" class="form-label">Confirm Password</label>
-        <input
-          class="form-control"
-          id="passwordConfirmInput"
-          type="password"
-          name="passwordConfirm"
-          placeholder="Password"
-        />
-      </div>
-      <button type="submit" class="btn btn-main">Register</button>
-    </form>
-    <br />
-    <small class="text-muted">
-      Already have an account? <a href="/login">Login</a>
-    </small>
+    </div>
   </div>
 </section>
