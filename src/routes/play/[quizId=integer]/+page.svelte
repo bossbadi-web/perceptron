a200c2ff9f95fb94ce8d50949447a75aa76d6ed5
Andy Tang <bossbadi123@gmail.com>
2023-10-20 23:34:25 -0400

    starting point for play page

--- src/routes/play/[quizId=integer]/+page.svelte
+++ src/routes/play/[quizId=integer]/+page.svelte
@@ -1,14 +1,20 @@
 <script>
-  export let data;
-
   import Mcq from "$lib/models/Mcq.svelte";
+
+  export let data;
+  const { quiz } = data;
 </script>
 
 <section>
   <div class="container">
     <div class="row">
       <div class="col-md-8 offset-md-2">
-        {#each data?.questions as question}
+        <div class="quiz-metadata">
+          <h1 class="quiz-title display-3">{quiz.title}</h1>
+          <p class="quiz-description lead">{quiz.description}</p>
+        </div>
+
+        {#each quiz?.data as question}
           <div class="question-box">
             <Mcq {question} />
           </div>
@@ -19,10 +25,21 @@
 </section>
 
 <style>
+  .quiz-metadata {
+    text-align: center;
+    margin-top: 2rem;
+    margin-bottom: 4rem;
+  }
+
+  .quiz-description {
+    font-size: 1.2rem;
+  }
+
   .question-box {
-    border: 1px solid black;
+    border: 1px solid gray;
+    box-shadow: 0 0 1rem rgba(0, 0, 0, 0.1);
     padding: 2rem;
-    margin-bottom: 1rem;
+    margin-bottom: 2rem;
     border-radius: 1rem;
   }
 
