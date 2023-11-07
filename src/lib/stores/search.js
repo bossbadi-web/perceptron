6684d5304fa2e30a8c1439da63842e919477b0a7
bossbadi <bossbadi123@gmail.com>
2023-11-07 10:59:00 -0500

    add sort by to explore

--- src/lib/stores/search.js
+++ src/lib/stores/search.js
@@ -7,7 +7,42 @@ export const createSearchStore = (data) => {
     search: "",
   });
 
-  return { subscribe, set, update };
+  const orderBy = (key, reverse = false) => {
+    update((store) => {
+      let sorted;
+
+      // alphabetically or reverse alphabetically
+      if (key === "A-Z") {
+        sorted = store.filtered.sort((a, b) => {
+          if (a.searchTerms < b.searchTerms) {
+            return reverse ? 1 : -1;
+          }
+          if (a.searchTerms > b.searchTerms) {
+            return reverse ? -1 : 1;
+          }
+          return 0;
+        });
+      } else {
+        // sort by id
+        sorted = store.filtered.sort((a, b) => {
+          if (a.id < b.id) {
+            return reverse ? 1 : -1;
+          }
+          if (a.id > b.id) {
+            return reverse ? -1 : 1;
+          }
+          return 0;
+        });
+      }
+
+      return {
+        ...store,
+        data: sorted,
+      };
+    });
+  };
+
+  return { subscribe, set, update, orderBy };
 };
 
 export const searchHandler = (store) => {
--- src/routes/explore/+page.svelte
+++ src/routes/explore/+page.svelte
@@ -16,6 +16,17 @@
   onDestroy(() => {
     unsubscribe();
   });
+
+  let orderByMessage = "Newest first";
+  const orderBy = (what, rev = false) => {
+    if (what === "A-Z") {
+      orderByMessage = rev ? "Z-A" : "A-Z";
+    } else if (what === "id") {
+      orderByMessage = rev ? "Newest first" : "Oldest first";
+    }
+
+    searchStore.orderBy(what, rev);
+  };
 </script>
 
 <section>
@@ -30,6 +41,46 @@
         <span>{$searchStore.data.length}</span>
       </p>
       <input type="text" class="form-control searchbar" placeholder="Search" bind:value={$searchStore.search} />
+    </div>
+
+    <div class="the-filters">
+      <div class="dropdown">
+        <button
+          class="btn btn-secondary dropdown-toggle"
+          type="button"
+          id="dropdownMenuButton1"
+          data-bs-toggle="dropdown"
+          aria-expanded="false"
+        >
+          {orderByMessage}
+        </button>
+        <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+          <li>
+            <button class="dropdown-item" on:click={() => orderBy("A-Z")}>
+              <i class="fas fa-sort-alpha-down" />
+              A-Z
+            </button>
+          </li>
+          <li>
+            <button class="dropdown-item" on:click={() => orderBy("A-Z", true)}>
+              <i class="fas fa-sort-alpha-down-alt" />
+              Z-A
+            </button>
+          </li>
+          <li>
+            <button class="dropdown-item" on:click={() => orderBy("id")}>
+              <i class="fas fa-person-cane" />
+              Oldest first
+            </button>
+          </li>
+          <li>
+            <button class="dropdown-item" on:click={() => orderBy("id", true)}>
+              <i class="fas fa-baby" />
+              Newest first
+            </button>
+          </li>
+        </ul>
+      </div>
     </div>
 
     <div class="all-cards">
