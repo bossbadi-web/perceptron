89c88be823581137f6eee32974bf24b36c82cabc
Andy Tang <bossbadi123@gmail.com>
2023-11-14 20:51:38 -0500

    add terms and privacy pages

---
+++ src/routes/(footer)/privacy/+page.svelte
@@ -0,0 +1,57 @@
+<section>
+  <div class="container">
+    <div class="col-md-10 offset-md-1">
+      <div class="row">
+        <h1 class="display-4 text-center">Privacy Policy</h1>
+
+        <p>
+          Throughout this document, "we", "us", and "our" refer to the creators of this website. "You" refers to the
+          user of this website. "Perceptron" refers to a quiz that you create on this website.
+        </p>
+
+        <h2>Data Collection</h2>
+        <p>When you sign up for an account, we collect your email address and the password that you choose.</p>
+
+        <p>When you create a Perceptron, we collect the title, description, and background image URL that you enter.</p>
+
+        <p>
+          You also have the option to upload an image of your notes. The image is processed by a <a
+            class="hover-underline"
+            href="https://ocr.space/ocrapi">third-party service</a
+          >
+          that extracts the text from the image. We use the extracted text and/or the additional notes that you enter to
+          generate quiz questions using another
+          <a class="hover-underline" href="https://api.gopubby.com/chatbot">third-party service</a>. Finally, we store
+          the generated quiz questions in our database.
+        </p>
+
+        <h2>Data Usage</h2>
+        <p>
+          We use your email address to send you a verification email when you register for an account. We use your email
+          address to send you a password reset email when you request a password reset. We do not use your email address
+          for any other purpose.
+        </p>
+
+        <h2>Data Storage</h2>
+        <p>
+          We store your email address and password in our Supabase database. The password is stored as a hash, which
+          means that we cannot see your password. We do not store your password in plain text. We do not store your
+          email address or password anywhere else.
+        </p>
+
+        <h2>Data Deletion</h2>
+        <p>To delete a Perceptron, you can click the "Delete" button on the edit page.</p>
+      </div>
+    </div>
+  </div>
+</section>
+
+<style>
+  h2 {
+    margin-top: 2rem;
+  }
+
+  p {
+    margin-bottom: 0.5rem;
+  }
+</style>
