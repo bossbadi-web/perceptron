328230225ec5b1135e9312f6e899f8f354d7b461
bossbadi <bossbadi123@gmail.com>
2023-11-15 10:36:38 -0500

    add last updated to terms and privacy

--- src/routes/(footer)/privacy/+page.svelte
+++ src/routes/(footer)/privacy/+page.svelte
@@ -4,17 +4,19 @@
       <div class="row">
         <h1 class="display-4 text-center">Privacy Policy</h1>
 
-        <p>
+        <p class="a-paragraph">
           Throughout this document, "we", "us", and "our" refer to the creators of this website. "You" refers to the
           user of this website. "Perceptron" refers to a quiz that you create on this website.
         </p>
 
-        <h2>Data Collection</h2>
-        <p>When you sign up for an account, we collect your email address and the password that you choose.</p>
-
-        <p>When you create a Perceptron, we collect the title, description, and background image URL that you enter.</p>
-
-        <p>
+        <h2 class="a-heading">Data Collection</h2>
+        <p class="a-paragraph">
+          When you sign up for an account, we collect your email address and the password that you choose.
+        </p>
+        <p class="a-paragraph">
+          When you create a Perceptron, we collect the title, description, and background image URL that you enter.
+        </p>
+        <p class="a-paragraph">
           You also have the option to upload an image of your notes. The image is processed by a <a
             class="hover-underline"
             href="https://ocr.space/ocrapi">third-party service</a
@@ -25,33 +27,39 @@
           the generated quiz questions in our database.
         </p>
 
-        <h2>Data Usage</h2>
-        <p>
+        <h2 class="a-heading">Data Usage</h2>
+        <p class="a-paragraph">
           We use your email address to send you a verification email when you register for an account. We use your email
           address to send you a password reset email when you request a password reset. We do not use your email address
           for any other purpose.
         </p>
 
-        <h2>Data Storage</h2>
-        <p>
+        <h2 class="a-heading">Data Storage</h2>
+        <p class="a-paragraph">
           We store your email address and password in our Supabase database. The password is stored as a hash, which
           means that we cannot see your password. We do not store your password in plain text. We do not store your
           email address or password anywhere else.
         </p>
 
-        <h2>Data Deletion</h2>
-        <p>To delete a Perceptron, you can click the "Delete" button on the edit page.</p>
+        <h2 class="a-heading">Data Deletion</h2>
+        <p class="a-paragraph">To delete a Perceptron, you can click the "Delete" button on the edit page.</p>
+
+        <p class="last-updated">Last updated November 15, 2023</p>
       </div>
     </div>
   </div>
 </section>
 
 <style>
-  h2 {
+  .a-heading {
     margin-top: 2rem;
   }
 
-  p {
+  .a-paragraph {
     margin-bottom: 0.5rem;
+  }
+
+  .last-updated {
+    margin-top: 2rem;
   }
 </style>
--- src/routes/(footer)/terms/+page.svelte
+++ src/routes/(footer)/terms/+page.svelte
@@ -4,93 +4,103 @@
       <div class="row">
         <h1 class="display-4 text-center">Terms of Service</h1>
 
-        <h2>Acceptable Use</h2>
-        <p>
+        <h2 class="a-heading">Acceptable Use</h2>
+        <p class="a-paragraph">
           You must not use this website in any way that causes, or may cause, damage to the website or impairment of the
           availability or accessibility of the website; or in any way which is unlawful, illegal, fraudulent or harmful,
           or in connection with any unlawful, illegal, fraudulent or harmful purpose or activity.
         </p>
 
-        <p>
+        <p class="a-paragraph">
           You must not use this website to copy, store, host, transmit, send, use, publish or distribute any material
           which consists of (or is linked to) any spyware, computer virus, Trojan horse, worm, keystroke logger, rootkit
           or other malicious computer software.
         </p>
 
-        <p>
+        <p class="a-paragraph">
           You must not conduct any systematic or automated data collection activities (including without limitation
           scraping, data mining, data extraction and data harvesting) on or in relation to this website without express
           written consent.
         </p>
 
-        <p>You must not use this website to transmit or send unsolicited commercial communications.</p>
+        <p class="a-paragraph">
+          You must not use this website to transmit or send unsolicited commercial communications.
+        </p>
 
-        <p>You must not use this website for any purposes related to marketing without express written consent.</p>
+        <p class="a-paragraph">
+          You must not use this website for any purposes related to marketing without express written consent.
+        </p>
 
-        <h2>Restricted Access</h2>
-        <p>
+        <h2 class="a-heading">Restricted Access</h2>
+        <p class="a-paragraph">
           Access to certain areas of this website is restricted. We reserve the right to restrict access to other areas
           of this website, or indeed this entire website, at our discretion.
         </p>
 
-        <p>
+        <p class="a-paragraph">
           If we provide you with a user ID and password to enable you to access restricted areas of this website or
           other content or services, you must ensure that the user ID and password are kept confidential.
         </p>
 
-        <p>
+        <p class="a-paragraph">
           We may disable your user ID and password at our sole discretion or if you breach any of the policies or terms
           governing your use of this website or any other contractual obligation you owe to us.
         </p>
 
-        <h2>Limitations of Liability</h2>
-        <p>
+        <h2 class="a-heading">Limitations of Liability</h2>
+        <p class="a-paragraph">
           The information on this website is provided free-of-charge, and you acknowledge that it would be unreasonable
           to hold us liable in respect of this website and the information on this website.
         </p>
 
-        <p>
+        <p class="a-paragraph">
           Whilst we endeavor to ensure that the information on this website is correct, we do not warrant its
           completeness or accuracy; nor do we not commit to ensuring that the website remains available or that the
           material on this website is kept up-to-date.
         </p>
 
-        <p>
+        <p class="a-paragraph">
           To the maximum extent permitted by applicable law we exclude all representations, warranties and conditions
           (including, without limitation, the conditions implied by law of satisfactory quality, fitness for purpose and
           the use of reasonable care and skill).
         </p>
 
-        <p>
+        <p class="a-paragraph">
           Our liability is limited and excluded to the maximum extent permitted under applicable law. We will not be
           liable for any direct, indirect or consequential loss or damage arising under these terms and conditions or in
           connection with our website, whether arising in tort, contract, or otherwise - including, without limitation,
           any loss of profit, contracts, business, goodwill, data, income, revenue or anticipated savings.
         </p>
 
-        <h2>Variation</h2>
-        <p>
+        <h2 class="a-heading">Variation</h2>
+        <p class="a-paragraph">
           We may revise these terms and conditions from time-to-time. Please check this page regularly to ensure you are
           familiar with the current version.
         </p>
 
-        <h2>Entire Agreement</h2>
-        <p>
+        <h2 class="a-heading">Entire Agreement</h2>
+        <p class="a-paragraph">
           These terms and conditions, together with our privacy policy constitute the entire agreement between you and
           us in relation to your use of our website, and supersede all previous agreements in respect of your use of
           this website.
         </p>
+
+        <p class="last-updated">Last updated November 15, 2023</p>
       </div>
     </div>
   </div>
 </section>
 
 <style>
-  h2 {
+  .a-heading {
     margin-top: 2rem;
   }
 
-  p {
+  .a-paragraph {
     margin-bottom: 0.5rem;
+  }
+
+  .last-updated {
+    margin-top: 2rem;
   }
 </style>
