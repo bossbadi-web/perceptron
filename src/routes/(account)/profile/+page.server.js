9dc632cde28db0e0da39a5925d1fe0348196f23e
bossbadi <bossbadi123@gmail.com>
2023-12-01 10:47:49 -0500

    improve logged in check for profile page

--- src/routes/(account)/profile/+page.server.js
+++ src/routes/(account)/profile/+page.server.js
@@ -1,13 +1,11 @@
 import { redirect } from "@sveltejs/kit";
 
-export const load = async ({ cookies, locals, url }) => {
-  const { error: err } = await locals.supabase.auth.getUser(cookies.get("access_token"));
+export const load = async ({ locals, url }) => {
+  const session = await locals.getSession();
 
-  if (err) {
+  if (!session) {
     throw redirect(303, `/login?redirectTo=${url.pathname}`);
   }
-
-  const session = await locals.getSession();
 
   return {
     email: session.user.email,
