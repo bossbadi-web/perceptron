2b47292b612e2e71fe92ee7e0d66196be083cf71
bossbadi <bossbadi123@gmail.com>
2024-01-23 10:52:08 -0500

    return to same scroll position after voting

---
+++ src/routes/(quizzes)/+layout.svelte
@@ -0,0 +1,19 @@
+<script>
+  import { beforeNavigate } from "$app/navigation";
+  import { onMount } from "svelte";
+
+  beforeNavigate(() => {
+    sessionStorage.setItem("scrollPosition", window.scrollY);
+    sessionStorage.setItem("previousURL", window.location.href);
+  });
+
+  onMount(() => {
+    const scrollPosition = sessionStorage.getItem("scrollPosition");
+    const previousURL = sessionStorage.getItem("previousURL");
+    if (scrollPosition && previousURL === window.location.href) {
+      window.scrollTo(0, scrollPosition);
+    }
+  });
+</script>
+
+<slot />
