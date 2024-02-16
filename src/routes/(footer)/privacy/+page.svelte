e5fd09f1c037037f66ca7a6f7b243b9d24c9b984
Andy Tang <bossbadi123@gmail.com>
2024-02-16 10:59:03 -0500

    add google stuff in privacy policy

--- src/routes/(footer)/privacy/+page.svelte
+++ src/routes/(footer)/privacy/+page.svelte
@@ -10,46 +10,57 @@
         </p>
 
         <h2 class="a-heading">Data Collection</h2>
-        <p class="a-paragraph">
-          When you sign up for an account, we collect your email address and the password that you choose.
-        </p>
-        <p class="a-paragraph">
-          When you create a Perceptron, we collect the title, description, and background image URL that you enter.
-        </p>
-        <p class="a-paragraph">
-          You also have the option to upload an image of your notes. The image is processed by a <a
-            class="hover-underline"
-            href="https://ocr.space/ocrapi">third-party service</a
-          >
-          that extracts the text from the image. We use the extracted text and/or the additional notes that you enter to
-          generate quiz questions using another
-          <a class="hover-underline" href="https://api.gopubby.com/chatbot">third-party service</a>. Finally, we store
-          the generated quiz questions in our database.
-        </p>
+        <ol>
+          <li class="a-paragraph">
+            When you register an account directly on the website form, we collect your email address, username, and
+            password. If you sign in with Google, we only collect your email address and username.
+          </li>
+          <li class="a-paragraph">
+            When you create a Perceptron, we collect the title, description, and background image URL that you enter.
+          </li>
+          <li class="a-paragraph">
+            You also have the option to upload an image of your notes. The image is processed by a <a
+              class="hover-underline"
+              href="https://ocr.space/ocrapi">third-party service</a
+            >
+            that extracts the text from the image. We use the extracted text and/or the additional notes that you enter to
+            generate quiz questions using another
+            <a class="hover-underline" href="https://api.gopubby.com/chatbot">third-party service</a>. Finally, we store
+            the generated quiz questions in our database.
+          </li>
+        </ol>
 
         <h2 class="a-heading">Data Usage</h2>
-        <p class="a-paragraph">
-          We use your email address to send you a verification email when you register for an account. We use your email
-          address to send you a password reset email when you request a password reset. We do not use your email address
-          for any other purpose.
-        </p>
+        <ol>
+          <li class="a-paragraph">
+            We use your email address to send you a verification email when you register for an account. We use your
+            email address to send you a password reset email when you request a password reset.
+          </li>
+          <li class="a-paragraph">
+            We use your username to associate you with your Perceptrons. It may be visible to other users.
+          </li>
+        </ol>
 
         <h2 class="a-heading">Data Storage</h2>
-        <p class="a-paragraph">
-          We store your email address and password in our Supabase database. The password is stored as a hash, which
-          means that we cannot see your password. We do not store your password in plain text. We do not store your
-          email address or password anywhere else.
-        </p>
+        <ol>
+          <li class="a-paragraph">We store your email address, username, and password in our Supabase database.</li>
+          <li class="a-paragraph">
+            The password is stored as a hash, which means that we cannot see your password. (If you signed in with
+            Google, we do not store a password.)
+          </li>
+        </ol>
 
         <h2 class="a-heading">Data Deletion</h2>
-        <p class="a-paragraph">To delete a Perceptron, you can click the "Delete" button on the edit page.</p>
-        <p class="a-paragraph">
-          To delete your account, you can click the "Delete Account" button on your profile page. This will delete all
-          of your Perceptrons and your account. We will not be able to recover your profile or your Perceptrons after
-          you delete them.
-        </p>
+        <ol>
+          <li class="a-paragraph">To delete a Perceptron, you can click the "Delete" button on the edit page.</li>
+          <li class="a-paragraph">
+            To delete your account, you can click the "Delete Account" button on your profile page. This will delete all
+            of your Perceptrons and your account. We will not be able to recover your profile or your Perceptrons after
+            you delete them.
+          </li>
+        </ol>
 
-        <p class="last-updated">Last updated November 30, 2023</p>
+        <p class="last-updated">Last updated February 16, 2024</p>
       </div>
     </div>
   </div>
@@ -62,6 +73,14 @@
 
   .a-paragraph {
     margin-bottom: 0.5rem;
+  }
+
+  ol {
+    margin-bottom: 0;
+  }
+
+  li {
+    margin-left: 1.5rem;
   }
 
   .last-updated {
