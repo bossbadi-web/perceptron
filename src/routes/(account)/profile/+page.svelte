74aaf5fdda9d2bb69584a9f7a3a5389569e1a077
Andy Tang <bossbadi123@gmail.com>
2024-03-21 08:37:21 -0400

    add color theme switcher

--- src/routes/(account)/profile/+page.svelte
+++ src/routes/(account)/profile/+page.svelte
@@ -1,29 +1,52 @@
 <script>
+  import { onMount } from "svelte";
   export let data;
+
+  function changeTheme(event) {
+    const theme = event.target.value;
+    document.documentElement.setAttribute("data-theme", theme);
+    localStorage.setItem("theme", theme);
+  }
+
+  var theme;
+  onMount(() => {
+    theme = localStorage.getItem("theme") ?? "light";
+  });
 </script>
 
 <section>
   <div class="container">
     <div class="col-md-8 offset-md-2">
       <div class="row normal-row">
-        <h1 class="display-4 text-center">My Profile</h1>
+        <form>
+          <h1 class="display-4 text-center">My Profile</h1>
 
-        <label for="username" class="form-label">Username</label>
-        <div class="input-group mb-3">
-          <input type="text" class="form-control text-muted" value={data.username} readonly />
-          <a class="btn btn-main" href="/username/change"><i class="fas fa-edit"></i></a>
-        </div>
+          <label for="username" class="form-label">Username</label>
+          <div class="input-group mb-3">
+            <input type="text" class="form-control text-muted" id="username" value={data.username} readonly />
+            <a class="btn btn-main" href="/username/change"><i class="fas fa-edit"></i></a>
+          </div>
 
-        <label for="email" class="form-label">Email</label>
-        <div class="input-group mb-3">
-          <input type="text" class="form-control text-muted" value={data.email} readonly />
-          <a class="btn btn-main" href="/email/change"><i class="fas fa-edit"></i></a>
-        </div>
+          <label for="email" class="form-label">Email</label>
+          <div class="input-group mb-3">
+            <input type="text" class="form-control text-muted" id="email" value={data.email} readonly />
+            <a class="btn btn-main" href="/email/change"><i class="fas fa-edit"></i></a>
+          </div>
 
-        <label for="id" class="form-label">ID</label>
-        <div class="input-group mb-3">
-          <input type="text" class="form-control text-muted" value={data.id} readonly />
-        </div>
+          <label for="id" class="form-label">ID</label>
+          <div class="input-group mb-3">
+            <input type="text" class="form-control text-muted" id="id" value={data.id} readonly />
+          </div>
+
+          <label for="theme" class="form-label">Color Theme</label>
+          <div class="input-group mb-3">
+            <select class="form-select" id="theme" value={theme} on:change={changeTheme}>
+              <option value="light">Light</option>
+              <option value="dark">Dark</option>
+              <option value="black">Black</option>
+            </select>
+          </div>
+        </form>
 
         <div class="buttons">
           <a class="btn btn-main" href="/password/change">Change Password</a>
