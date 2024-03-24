1c8d8c49f82eb6e6d0d41b8f8b065b49787a62cb
Andy Tang <bossbadi123@gmail.com>
2024-03-24 02:49:27 -0400

    remove header comment from layout.js

--- src/routes/+layout.js
+++ src/routes/+layout.js
@@ -1,4 +1,3 @@
-// src/routes/+layout.js
 import { PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY } from "$env/static/public";
 import { createSupabaseLoadClient } from "@supabase/auth-helpers-sveltekit";
 import { completeUser } from "$lib/utils";
