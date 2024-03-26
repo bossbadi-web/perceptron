e529a47726a187e961792e124e672871bdafaacc
Andy Tang <bossbadi123@gmail.com>
2024-03-25 22:58:37 -0400

    remove unused url parameter

--- src/routes/(account)/email/change/+page.server.js
+++ src/routes/(account)/email/change/+page.server.js
@@ -2,7 +2,7 @@ import { fail } from "@sveltejs/kit";
 import { redirect, setFlash } from "sveltekit-flash-message/server";
 
 export const actions = {
-  default: async ({ cookies, request, locals, url }) => {
+  default: async ({ cookies, request, locals }) => {
     const formData = await request.formData();
 
     // check password
