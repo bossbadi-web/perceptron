abacea508112f7ff0a475ad43c294d8e6f9634f9
Andy Tang <bossbadi123@gmail.com>
2024-03-29 17:02:57 -0400

    update landing questions

--- src/lib/landingQuestions.js
+++ src/lib/landingQuestions.js
@@ -2,31 +2,43 @@
 
 export const LANDING_QUESTIONS = [
   {
-    question: "What is the world's largest ocean by surface area?",
-    options: ["Atlantic Ocean", "Indian Ocean", "Arctic Ocean", "Pacific Ocean"],
+    question: "What is the value of π to 2 decimal places?",
+    options: ["2.21", "3.14", "4.13", "5.15"],
   },
   {
-    question: 'Which planet in our solar system is known as the "Red Planet"?',
-    options: ["Venus", "Mars", "Jupiter", "Saturn"],
+    question: "What force is responsible for keeping the planets in orbit around the Sun?",
+    options: ["Electromagnetic force", "Gravitational force", "Nuclear force", "Frictional force"],
   },
   {
-    question: "What is the chemical symbol for the element gold?",
-    options: ["Go", "Au", "Ag", "GoLd"],
+    question: "What is the process by which plants make their food called?",
+    options: ["Respiration", "Photosynthesis", "Digestion", "Transpiration"],
   },
   {
-    question: "Which country is known as the Land of the Rising Sun?",
-    options: ["China", "South Korea", "Japan", "Thailand"],
+    question: "What is the chemical symbol for gold?",
+    options: ["Ag", "Au", "Ge", "Ga"],
   },
   {
-    question: "Who painted the Mona Lisa?",
-    options: ["Pablo Picasso", "Vincent van Gogh", "Leonardo da Vinci", "Claude Monet"],
+    question: "Who was the first President of the United States?",
+    options: ["Abraham Lincoln", "George Washington", "Thomas Jefferson", "John Adams"],
   },
   {
-    question: "What is the smallest prime number?",
-    options: ["0", "1", "2", "3"],
+    question: "What is the longest river in the world?",
+    options: ["Amazon", "Nile", "Yangtze", "Mississippi"],
   },
   {
-    question: "What is the largest organ in the human body?",
-    options: ["Heart", "Brain", "Liver", "Skin"],
+    question: 'Which of the following is a synonym for "happy"?',
+    options: ["Sad", "Elated", "Tired", "Anxious"],
+  },
+  {
+    question: "Which of these is a primary color?",
+    options: ["Green", "Orange", "Blue", "Purple"],
+  },
+  {
+    question: "What is the term for a play in soccer where a player kicks the ball to a teammate?",
+    options: ["Dribble", "Tackle", "Pass", "Shoot"],
+  },
+  {
+    question: "Which of the following is a programming language?",
+    options: ["HTML", "HTTP", "Java", "FTP"],
   },
 ];
