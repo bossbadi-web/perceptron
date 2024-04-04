bed28fe4089c0019c67caca18a2577f1b3e3eb4d
bossbadi <bossbadi123@gmail.com>
2024-04-04 10:53:59 -0400

    toast message when logging out

--- src/routes/(account)/logout/+server.js
+++ src/routes/(account)/logout/+server.js
@@ -1,11 +1,12 @@
-import { error, redirect } from "@sveltejs/kit";
+import { error } from "@sveltejs/kit";
+import { redirect } from "sveltekit-flash-message/server";
 
-export const POST = async ({ locals }) => {
+export const POST = async ({ cookies, locals }) => {
   const { error: err } = await locals.supabase.auth.signOut();
 
   if (err) {
     throw error(500, "Something went wrong logging you out.");
   }
 
-  throw redirect(303, "/");
+  throw redirect(303, "/", { type: "success", message: "Logged out of all devices." }, cookies);
 };
