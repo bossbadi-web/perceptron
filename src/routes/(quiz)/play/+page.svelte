ccf90f58c6fafa71258965e4b8ccca808d77a758
Andy Tang <bossbadi123@gmail.com>
2024-04-06 18:07:08 -0400

    delete edit, play, and preview default pages

--- src/routes/(quiz)/play/+page.svelte
+++
@@ -1,6 +0,0 @@
-<section>
-  <div class="container">
-    <h1>this is the quiz page</h1>
-    <a href="/">Go home</a>
-  </div>
-</section>
--- src/routes/(quiz)/preview/+page.svelte
+++
@@ -1,6 +0,0 @@
-<section>
-  <div class="container">
-    <h1>this is the preview page</h1>
-    <a href="/">Go home</a>
-  </div>
-</section>
