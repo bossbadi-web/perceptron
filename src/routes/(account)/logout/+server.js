79092ac8276eca9e605fff1c4088911cd1c2d960
bossbadi <bossbadi123@gmail.com>
2024-05-07 10:46:53 -0400

    force logout if it errors

--- src/routes/(account)/logout/+server.js
+++ src/routes/(account)/logout/+server.js
@@ -1,12 +1,13 @@
-import { error } from "@sveltejs/kit";
 import { redirect } from "sveltekit-flash-message/server";
 
 export const POST = async ({ cookies, locals }) => {
+  let redirectUrl = "/";
+
   const { error: err } = await locals.supabase.auth.signOut();
 
   if (err) {
-    throw error(500, "Something went wrong logging you out.");
+    redirectUrl = "/?signOut";
   }
 
-  throw redirect(303, "/", { type: "success", message: "Logged out of all devices." }, cookies);
+  throw redirect(303, redirectUrl, { type: "success", message: "Logged out of all devices." }, cookies);
 };
